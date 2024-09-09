975c9aedb9c0f3204d705adbaebb001f6e9681ac
Andrii Lisnyak <andrey.lisnyak@gmail.com>
2024-09-09 20:29:54 +0300

    Add initial Vue.js project setup with Tailwind CSS
    This commit sets up a new Vue.js project with Tailwind CSS integration. It includes essential components, configurations, and dependencies such as Vue, Tailwind, ESLint, and Prettier. This lays the groundwork for further development.

---
+++ lec2-ex/src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+
+import Navigation from "@/components/Navigation.vue";
+import Alert from "@/components/Alert.vue";
+</script>
+
+<template>
+    <Navigation/>
+    <main class="container mx-auto">
+        <div class="flex flex-col p-4">
+            <Alert v-for="type in ['info','danger', 'warning', 'success', 'error']" :key="type" :message="`This is an ${type} alert`" :type="type"/>
+        </div>
+    </main>
+
+</template>
---
+++ lec2-ex/src/components/Alert.vue
@@ -0,0 +1,43 @@
+<script setup>
+import {computed} from "vue";
+
+const props = defineProps({
+    message: String,
+    type: {
+        type: String,
+        default: 'info'
+    }
+})
+const wrapperClasses = computed(() => {
+    return {
+        alert: true,
+        [`alert--${props.type}`]: props.type,
+    }
+})
+</script>
+
+<template>
+    <div :class="wrapperClasses" role="alert">
+        {{ message }}
+    </div>
+</template>
+
+<style lang="scss">
+.alert {
+    &--info {
+        @apply p-4 mb-4 text-sm text-blue-800 rounded-lg bg-blue-50 dark:bg-gray-800 dark:text-blue-400;
+    }
+    &--danger{
+        @apply p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400
+    }
+    &--success {
+        @apply p-4 mb-4 text-sm text-green-800 rounded-lg bg-green-50 dark:bg-gray-800 dark:text-green-400;
+    }
+    &--warning {
+        @apply p-4 mb-4 text-sm text-yellow-800 rounded-lg bg-yellow-50 dark:bg-gray-800 dark:text-yellow-300;
+    }
+    &--error {
+        @apply p-4 text-sm text-gray-800 rounded-lg bg-gray-50 dark:bg-gray-800 dark:text-gray-300;
+    }
+}
+</style>
